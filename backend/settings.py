3ef1d0e15edd5446ac61c62054025db7eb859756
Shreya Anand <anandshreya1803@gmail.com>
2026-05-29 15:39:56 +0530

    Switch email service to Brevo

--- backend/settings.py
+++ backend/settings.py
@@ -116,12 +116,12 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_HOST = "smtp-relay.brevo.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("BREVO_USER")
+EMAIL_HOST_PASSWORD = os.getenv("BREVO_PASSWORD")
 
 EMAIL_TIMEOUT = 10
 
